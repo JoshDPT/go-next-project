1f54bcf107dbffb4f2821889168ac174e3883b02
JoshDPT <jjewcu@gmail.com>
2023-03-06 22:11:39 -0500

    websocket progress

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,71 @@
 package main
-
+ 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
+
+		"github.com/gorilla/websocket"
 )
+ 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// print out that message for clarity
+		log.Println(string(p))
+ 
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+ 
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	// upgrade this connection to a WebSocket
+	// connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+ 
+	log.Println("Client Connected")
+	err = ws.WriteMessage(1, &#91;]byte("Hi Client!"))
+	if err != nil {
+		log.Println(err)
+	}
+	// listen indefinitely for new messages coming
+	// through on our WebSocket connection
+	reader(ws)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Home HTTP")
+}
+ 
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello WebSocket")
+}
+ 
+func setupRoutes() {
+    http.HandleFunc("/", homePage)
+    http.HandleFunc("/ws", wsEndpoint)
+}
+ 
 func main() {
-	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		resp := []byte(`{"status": "ok"}`)
-		rw.Header().Set("Content-Type", "application/json")
-		rw.Header().Set("Content-Length", fmt.Sprint(len(resp)))
-		rw.Write(resp)
-	})
-
-	log.Println("Server is available at http://localhost:8000")
-	log.Fatal(http.ListenAndServe(":8000", handler))
+    fmt.Println("Hello World")
+    setupRoutes()
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,11 @@
 export default async function Home() {
-  const {status} = await fetch("http://localhost:8000/status").then(x=> x.json());
+	const { status } = await fetch('http://localhost:8000/status').then((x) =>
+		x.json()
+	);
 
-  return (
-    <div className='text-3xl font-extrabold underline'>
-      Status is {status}
-    </div>
-  );
+	return (
+		<div className="flex text-3xl font-extrabold underline justify-center place-items-center">
+			<h1>Status is {status}</h1>
+		</div>
+	);
 }
